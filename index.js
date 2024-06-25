c68bb73d02655e10c8e7549e0cc68616b9cc0398
Collins63 <collinskurai@gmail.com>
2024-06-25 12:18:00 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const port = 3000
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
+const jobRouter = require('./routes/job')
+const bodyParser = require('body-parser')
+
+dotenv.config();
+mongoose.connect(process.env.MONGO_URL).then(()=> console.log('connected to sparkle')).catch((err)=>console.log(err));
+
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended: true}))
+
+app.use('/api/jobs' , jobRouter)
+
+
+app.listen(process.env.PORT  || PORT, () => {
+  console.log(`Server is running on port ${process.env.PORT}`);
+});
