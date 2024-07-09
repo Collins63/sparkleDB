ecbbc18ad675750f1b2ad59b09addda1a2df232e
Collins63 <collinskurai@gmail.com>
2024-07-09 11:51:23 +0200

    continuation

--- models/Bookmark.js
+++ models/Bookmark.js
@@ -1,18 +1,19 @@
 const mongoose =require('mongoose');
 
-const JobSchema  = new mongoose.Schema({
-    
-    title:{type: String, required: true},
+const BookMarkSchema  = new mongoose.Schema({
     location:{type: String, required: true},
+    location:{type: String, required: true},
+    title:{type: String, required: true},
     description:{type: String, required: true},
-    company: {type:String , required:false},
+    agentName:{type: String, required: true},
     salary:{type: String, required: true}, 
     period:{type: String, required: true},
     contract:{type: String, required: true},
+    hiring:{type: Boolean, required: true , default:true},
     requirements:{
         type: Array,
         required: true},
     imageUrl:{type: String, required: true},
     agentId:{type: String, required: true},
 },{timestamps: true});
-module.exports = mongoose.model('Job', JobSchema)
+module.exports = mongoose.model('Bookmark', BookMarkSchema)
---
+++ models/User.js
@@ -0,0 +1,19 @@
+const mongoose = require("mongoose");
+const UserSchema = new mongoose.Schema(
+    {
+        username:{type: String , required: true , unique:true},
+        email:{type: String , required: true , unique:true},
+        password:{type: String , required: true},
+        location:{type: String , required: true },
+        isAdmin:{type: Boolean , default:false},
+        isAgent:{type: Boolean , default:false},
+        skills:{type: Array , default:false },
+        profile:{
+            type: String,
+            required: true,
+            default:"https://img.icons8.com/officel/80/user.png",
+        }
+    }
+);
+
+module.exports = mongoose.model("User" , UserSchema)
